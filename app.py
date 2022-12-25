61451a45e3e6a86e2cd13867060f9c86c568cfb4
Avaneesh Sathish <avaneesh.sathish@gmail.com>
2022-12-25 15:25:21 -0500

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
@@ -33,3 +32,6 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
+
+
